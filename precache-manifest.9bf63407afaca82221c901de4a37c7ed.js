eb57bd6832b7a3b5e7a6c359dfee5d32e12d4684
maneeshjapa <jmaneeshh@gmail.com>
2020-03-13 18:19:12 +0530

    Updates

---
+++ precache-manifest.9bf63407afaca82221c901de4a37c7ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72233f8d335830f938b0ef26417bbec8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4fe6d1e5c4b71da5f35a",
+    "url": "/monsters-rolodex/static/css/main.020d9d1c.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe6d1e5c4b71da5f35a",
+    "url": "/monsters-rolodex/static/js/main.484c9e45.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
